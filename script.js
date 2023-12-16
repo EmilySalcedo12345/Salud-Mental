f8af2107224bd1ca5479138bd46fb4938bebf574
EmilySalcedo12345 <120775640+EmilySalcedo12345@users.noreply.github.com>
2023-12-15 21:30:45 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -4,25 +4,25 @@ const chatbox = document.querySelector(".chatbox");
 const chatInput = document.querySelector(".chat-input textarea");
 const sendChatBtn = document.querySelector(".chat-input span");
 
-let userMessage = null; // Variable para el msg
-const API_KEY = "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"; // mi API
+let userMessage = null; // Variable para almacenar el mensaje del usuario.
+const API_KEY = "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"; // MI API KEY
 const inputInitHeight = chatInput.scrollHeight;
 
 const createChatLi = (message, className) => {
-    // Crear un elemento de chat <li> con el mensaje pasado y className
+    // Cree un elemento de chat <li> con el mensaje pasado y el nombre de clase
     const chatLi = document.createElement("li");
     chatLi.classList.add("chat", `${className}`);
     let chatContent = className === "outgoing" ? `<p></p>` : `<span class="material-symbols-outlined">smart_toy</span><p></p>`;
     chatLi.innerHTML = chatContent;
     chatLi.querySelector("p").textContent = message;
-    return chatLi; // return chat <li> 
+    return chatLi; // retornar chat <li> element
 }
 
 const generateResponse = (chatElement) => {
     const API_URL = "https://api.openai.com/v1/chat/completions";
     const messageElement = chatElement.querySelector("p");
 
-    // propiedas y mensajes para el API
+    // Definir las propiedades y el mensaje para la solicitud de API.
     const requestOptions = {
         method: "POST",
         headers: {
@@ -36,29 +36,29 @@ const generateResponse = (chatElement) => {
         })
     }
 
-    // Envíe la solicitud POST a la API, obtenga una respuesta y establezca la respuesta como texto de párrafo
+    // Envíe una solicitud POST a la API, obtenga una respuesta y establezca la respuesta como texto de párrafo
     fetch(API_URL, requestOptions).then(res => res.json()).then(data => {
         messageElement.textContent = data.choices[0].message.content.trim();
     }).catch(() => {
         messageElement.classList.add("error");
-        messageElement.textContent = "Oops! Something went wrong. Please try again.";
+        messageElement.textContent = "Algo salió mal. Por favor, inténtelo de nuevo.";
     }).finally(() => chatbox.scrollTo(0, chatbox.scrollHeight));
 }
 
 const handleChat = () => {
-    userMessage = chatInput.value.trim(); // Obtener el mensaje ingresado por el usuario y eliminar espacios en blanco adicionales
+    userMessage = chatInput.value.trim(); // Obtenemos mensaje ingresado por el usuario y elimine los espacios en blanco adicionales
     if (!userMessage) return;
 
-    // Borre el área de texto de entrada y establezca su altura en predeterminada
+    // Borre el área de texto de entrada y establezca su altura por defecto
     chatInput.value = "";
     chatInput.style.height = `${inputInitHeight}px`;
 
-    // Anexar el mensaje del usuario al cuadro de chat
+    // Agregar el mensaje al chatbox
     chatbox.appendChild(createChatLi(userMessage, "outgoing"));
     chatbox.scrollTo(0, chatbox.scrollHeight);
 
     setTimeout(() => {
-        // Mostrar "Pensando..." mensaje mientras espera la respuesta
+        // Mostrar el mensaje "Pensando..." mientras espera la respuesta
         const incomingChatLi = createChatLi("Pensando...", "incoming");
         chatbox.appendChild(incomingChatLi);
         chatbox.scrollTo(0, chatbox.scrollHeight);
@@ -67,14 +67,14 @@ const handleChat = () => {
 }
 
 chatInput.addEventListener("input", () => {
-    // Ajustar la altura del área de texto de entrada en función de su contenido
+    // Ajustar la altura del área de texto de entrada según su contenido
     chatInput.style.height = `${inputInitHeight}px`;
     chatInput.style.height = `${chatInput.scrollHeight}px`;
 });
 
 chatInput.addEventListener("keydown", (e) => {
-    // Si se pulsa la tecla Intro sin la tecla Mayús y la ventana 
-    // El ancho es superior a 800 px, maneja el chat
+    // Si se presiona la tecla Enter sin la tecla Shift y la ventana
+    // el ancho es mayor a 800px, maneja el chat
     if (e.key === "Enter" && !e.shiftKey && window.innerWidth > 800) {
         e.preventDefault();
         handleChat();
